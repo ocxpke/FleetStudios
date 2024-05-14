9c668ea9075feb907bf2cef93d0f766cc9908370
ocxpke <joseramirez0707@gmail.com>
2024-05-14 03:27:36 +0200

    resize & background

--- fleetStudios/src/main/java/com/packages/FleetStudios/Primera_Capa.java
+++ fleetStudios/src/main/java/com/packages/FleetStudios/Primera_Capa.java
@@ -13,6 +13,7 @@ import java.awt.Color;
 import java.awt.SystemColor;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 
 public class Primera_Capa extends JFrame {
 
@@ -40,15 +41,16 @@ public class Primera_Capa extends JFrame {
 	 */
 	public Primera_Capa() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(250, 50, 1100, 700);
+		setBounds(250, 50, 1115, 740);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(0, 204, 204));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		
-		JButton inicioSesion = new JButton("Iniciar Sesión");
-		inicioSesion.setBounds(447, 237, 190, 65);
+		JButton inicioSesion = new JButton("");
+		inicioSesion.setIcon(new ImageIcon("C:\\Users\\ocXpke\\Downloads\\Iniciar sesion.png"));
+		inicioSesion.setBounds(375, 351, 350, 80);
 		inicioSesion.setFont(new Font("Arial Black", Font.PLAIN, 15));
 		inicioSesion.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -60,8 +62,9 @@ public class Primera_Capa extends JFrame {
 		contentPane.setLayout(null);
 		contentPane.add(inicioSesion);
 		
-		JButton creaUnaCuenta = new JButton("Crea tu Cuenta");
-		creaUnaCuenta.setBounds(447, 345, 190, 65);
+		JButton creaUnaCuenta = new JButton("");
+		creaUnaCuenta.setIcon(new ImageIcon("C:\\Users\\ocXpke\\Downloads\\Diseño sin título (1).png"));
+		creaUnaCuenta.setBounds(375, 429, 350, 80);
 		creaUnaCuenta.setFont(new Font("Arial Black", Font.PLAIN, 15));
 		creaUnaCuenta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
