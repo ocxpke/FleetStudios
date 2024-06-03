dc223a6177b472accb2955e5096832ba12ebb916
arodsam <164351740+arodsam@users.noreply.github.com>
2024-06-03 19:03:08 +0200

    Incluidas pruebas unitarias para Crear Cuenta

--- fleetStudios/src/test/java/com/packages/FleetStudios/AppTest.java
+++ fleetStudios/src/test/java/com/packages/FleetStudios/AppTest.java
@@ -1,6 +1,23 @@
 package com.packages.FleetStudios;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import org.junit.Test;
 
@@ -12,9 +29,49 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @BeforeEach
+    public void init() {
+        
     }
+    
+    @AfterEach
+    public void terminate() {
+        
+    }
+    @Test
+    public void testCrearCuentaElNickNoEstaVacio() {
+        CrearCuenta cc = new CrearCuenta(null);
+        cc.nick.setText("");
+        cc.email.setText("email@example.com");
+        cc.fecNac.setText("1990-01-01");
+        cc.contra.setText("password");
+
+        String resultado = cc.crearCuenta();
+        assertEquals("El campo nick no puede estar vacío", resultado);
+    }
+    
+    @Test
+   public void testCrearCuentaElCorreoNoEstaVacio() {
+        CrearCuenta cc = new CrearCuenta(null);
+        cc.nick.setText("nick");
+        cc.email.setText("");
+        cc.fecNac.setText("1990-01-01");
+        cc.contra.setText("password");
+
+        String resultado = cc.crearCuenta();
+        assertEquals("El campo correo electrónico no puede estar vacío", resultado);
+    }
+
+    @Test
+   public void testCrearCuentaLaContrasenaNoEstaVacia() {
+        CrearCuenta cc = new CrearCuenta(null);
+        cc.nick.setText("nick");
+        cc.email.setText("email@example.com");
+        cc.fecNac.setText("1990-01-01");
+        cc.contra.setText("");
+
+        String resultado = cc.crearCuenta();
+        assertEquals("El campo contraseña no puede estar vacío", resultado);
+    }
+     
 }
